5db024dfcb4fe54c99f87ddb4e28d4ebff78b097
alan3028 <santiagodavidv135@gmail.com>
2025-06-13 18:52:06 -0600

    practica 7

--- flaskproyec/app.py
+++ flaskproyec/app.py
@@ -1,6 +1,25 @@
-from flask import Flask 
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']="alandavid10"
+app.config['MYSQL_DB']="dbflask"
+
+mysql=MySQL(app)
+
+#ruta para probar la coneccion a MYSQL
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify({'status':'ok','message':'Conectado con exito '}),200
+    except MySQLdb.MySQLError as e :
+          return jsonify({'status':'error','message':str(e)}),500
 
 #ruta simple
 @app.route('/')
